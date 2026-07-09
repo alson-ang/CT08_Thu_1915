5b15e91dc5153ea0e100f4e63179d5aa6866ffff
alson-ang <alson.ang@computhink.com.sg>
2026-07-09 12:03:17 +0000

    Jul 9, 2026, 12:03 PM

--- basic.js
+++ basic.js
@@ -2,7 +2,7 @@ function setup() {
     new Canvas(800, 400);
     background(220);
 
-    fill(0);    // Text
+    fill(0);    // Text colour
     textSize(24);
     
     for (let i = 10; i > 0; i - 2) {
