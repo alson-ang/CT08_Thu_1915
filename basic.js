d1a3bfc26b80b53d45b76c3b9b91c3bf117f5005
alson-ang <alson.ang@computhink.com.sg>
2026-07-09 12:20:37 +0000

    Jul 9, 2026, 12:20 PM

--- basic.js
+++ basic.js
@@ -5,28 +5,28 @@ function setup() {
     fill("black");    // Text colour
     textSize(24);   // Text size
     
-    for (let i = 2; i <= 20; i += 2) {
-        console.log(i);
-        yPos = 10 + (i * 10)
-        text(i, 100, yPos)
-    }
+    // for (let i = 2; i <= 20; i += 2) {
+    //     console.log(i);
+    //     yPos = 10 + (i * 10)
+    //     text(i, 100, yPos)
+    // }
 
-    let age = 13;
+    // let age = 13;
 
-    if (age <= 9) {
-        console.log("Lower Primary");
-    } else if (age <= 12) {
-        console.log("Upper Primary");
-    } else {
-        console.log("Secondary");
-    }
+    // if (age <= 9) {
+    //     console.log("Lower Primary");
+    // } else if (age <= 12) {
+    //     console.log("Upper Primary");
+    // } else {
+    //     console.log("Secondary");
+    // }
 
-    let count = 0;
+    // let count = 0;
 
-    while (count < 10) {
-        console.log(count);
-        count++;
-    }
+    // while (count < 10) {
+    //     console.log(count);
+    //     count++;
+    // }
 
     let groceries = ["apple", "bread", "milk"];
 
