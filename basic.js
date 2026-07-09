53d3438f7c08b7471e97b3b9c68d28103f746a7d
alson-ang <alson.ang@computhink.com.sg>
2026-07-09 11:48:54 +0000

    Jul 9, 2026, 11:48 AM

--- basic.js
+++ basic.js
@@ -1,11 +1,11 @@
 function setup() {
     new Canvas(800, 400)
     background(220)
-    console.log("Hello!")
+
     let base = 10
     let height = 5
     let area = 0.5 * base * height
-    console.log("Area:")
+    console.log("Area:", area)
 }
 
 function draw() {
