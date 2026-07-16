8561b946c3e3386b8680a514fcd24f19fe44a5fb
alson-ang <alson.ang@computhink.com.sg>
2026-07-16 12:17:22 +0000

    Jul 16, 2026, 12:17 PM

--- 02_geometrydash/geodash_main.js
+++ 02_geometrydash/geodash_main.js
@@ -28,7 +28,7 @@ function setup() {
   world.gravity.y = 32;
 
   // Create player sprite
-  startCoordinate = []; // start_coordinate - snake case ; 
+  startCoordinate = []; // start_coordinate - snake case ; oneWord - camelCast
   player = new Sprite(50, height, 50 ,50);  // (x, y, width, height)
   player.img = cube;
   player.friction = 0;
