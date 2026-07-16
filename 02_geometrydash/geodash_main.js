01ddf3c26b7539809babb1e3b3b4b49a5c49263a
alson-ang <alson.ang@computhink.com.sg>
2026-07-16 12:16:50 +0000

    Jul 16, 2026, 12:16 PM

--- 02_geometrydash/geodash_main.js
+++ 02_geometrydash/geodash_main.js
@@ -28,7 +28,7 @@ function setup() {
   world.gravity.y = 32;
 
   // Create player sprite
-  startCoordinate = [];
+  startCoordinate = []; //
   player = new Sprite(50, height, 50 ,50);  // (x, y, width, height)
   player.img = cube;
   player.friction = 0;
