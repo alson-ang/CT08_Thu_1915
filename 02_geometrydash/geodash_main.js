37f18239cb2cd4d7d1530f0534812812d6bc41ed
alson-ang <alson.ang@computhink.com.sg>
2026-07-23 11:49:29 +0000

    Jul 23, 2026, 11:49 AM

--- 02_geometrydash/geodash_main.js
+++ 02_geometrydash/geodash_main.js
@@ -72,7 +72,7 @@ function setup() {
   finishLine = new Group();
   finishLine.tile = "f";
   finishLine.w = TILE_SIZE;
-  finishLine.h = height;
+  finishLine.h = height * 2;
   finishLine.collider = "static";
   finishLine.color = "orange"
 
