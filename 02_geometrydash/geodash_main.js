b60a3a7893096d926b916af1d7695abc1d9f853f
alson-ang <alson.ang@computhink.com.sg>
2026-07-23 11:23:23 +0000

    Jul 23, 2026, 11:23 AM

--- 02_geometrydash/geodash_main.js
+++ 02_geometrydash/geodash_main.js
@@ -49,7 +49,7 @@ function setup() {
   ground.w = TILE_SIZE; // Width
   ground.h = TILE_SIZE; // Height
   ground.color = "black";
-  ground.collider = "static";
+  ground.collider = "static"; // dynamic
 }
 
 function draw() {
