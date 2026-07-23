d9943e2b5f40a438a45db7e569f889f5fee4b34e
alson-ang <alson.ang@computhink.com.sg>
2026-07-23 12:18:25 +0000

    Jul 23, 2026, 12:18 PM

--- 02_geometrydash/geodash_main.js
+++ 02_geometrydash/geodash_main.js
@@ -97,7 +97,7 @@ function draw() {
   }
 
   // Jump input
-  if (kb.presses("space") || mouse.presses("left")) {
+  if ((kb.presses("space") || mouse.presses("left"))) {
     player.vel.y = -8;  // Negative y = moving up
     player.rotateTo(player.rotation + 359, 15); // (direction, speed)
   }
