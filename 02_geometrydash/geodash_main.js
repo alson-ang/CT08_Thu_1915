04f5cbc844a1a6c8b4c678149fc495e7cfcf32e9
alson-ang <alson.ang@computhink.com.sg>
2026-07-23 11:34:15 +0000

    Jul 23, 2026, 11:34 AM

--- 02_geometrydash/geodash_main.js
+++ 02_geometrydash/geodash_main.js
@@ -57,6 +57,7 @@ function setup() {
   spikes.tile = "s";
   spikes.w = TILE_SIZE;
   spikes.h = TILE_SIZE;
+  
 
   new Tiles(tileMap1, 0, 0, TILE_SIZE, TILE_SIZE);  // Create tiles from tileMap1
 }
