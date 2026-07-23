25ce9b65a5fec7b73e9b1f1e0786337534687a6f
alson-ang <alson.ang@computhink.com.sg>
2026-07-23 11:45:56 +0000

    Jul 23, 2026, 11:45 AM

--- 02_geometrydash/geodash_main.js
+++ 02_geometrydash/geodash_main.js
@@ -72,7 +72,7 @@ function setup() {
   finishLine = new Group();
   finishLine.tile = "f";
   finishLine.w = TILE_SIZE;
-  
+  finishLine.h = height
 
   new Tiles(tileMap1, 0, 0, TILE_SIZE, TILE_SIZE);  // Create tiles from tileMap1
 }
