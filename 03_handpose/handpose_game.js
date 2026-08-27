e0154111abb6ff24b47e24504eefe060559dcf62
alson-ang <alson.ang@computhink.com.sg>
2026-08-27 12:35:40 +0000

    Aug 27, 2026, 12:35 PM

--- 03_handpose/handpose_game.js
+++ 03_handpose/handpose_game.js
@@ -45,7 +45,7 @@ function setup() {
   // Game sprite
   fingerTip = new Sprite();
   fingerTip.diameter = 60;
-  fingerTip.collider = "dynamic"
+  fingerTip.collider = "static"
 }
 
 function draw() {
