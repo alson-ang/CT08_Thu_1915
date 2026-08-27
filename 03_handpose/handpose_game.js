4cdb80ebac585da710cdef3f0df5ab89aa24d75c
alson-ang <alson.ang@computhink.com.sg>
2026-08-27 12:37:25 +0000

    Aug 27, 2026, 12:37 PM

--- 03_handpose/handpose_game.js
+++ 03_handpose/handpose_game.js
@@ -46,7 +46,7 @@ function setup() {
   fingerTip = new Sprite();
   fingerTip.diameter = 60;
   fingerTip.collider = "kinematic"
-  fingerTip.color = "rgba"
+  fingerTip.color = "rgba(0, 255, 0, 0,05"
 }
 
 function draw() {
