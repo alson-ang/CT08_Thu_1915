a111022b9b0e7fd9a86f997612d2c99a3959058e
alson-ang <alson.ang@computhink.com.sg>
2026-08-27 11:27:34 +0000

    Aug 27, 2026, 11:27 AM

--- basic.js
+++ basic.js
@@ -14,8 +14,8 @@ function setup() {
     let age = 13;
 
     if (age <= 9) {
-        console.log("Lower Primary");
-    } else if (age <= 12) { // elif
+        console.log("Lower Primary"); // print
+    } else if (age <= 12) {
         console.log("Upper Primary");
     } else {
         console.log("Secondary");
