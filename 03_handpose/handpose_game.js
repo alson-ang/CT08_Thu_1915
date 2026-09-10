0ca932b40aa110adc510493548a4098f2bcec86e
alson-ang <alson.ang@computhink.com.sg>
2026-09-10 11:42:01 +0000

    Sep 10, 2026, 11:42 AM

--- 03_handpose/handpose_game.js
+++ 03_handpose/handpose_game.js
@@ -53,7 +53,7 @@ function setup() {
   balloon = new Sprite();
   balloon.diameter = 80;
   balloon.collider = "dynamic";
-  fingerTip.color = "rgba(255, 225, 0, 0.05)";
+  fingerTip.color = "rgb(255, 225, 0)";
 }
 
 function draw() {
