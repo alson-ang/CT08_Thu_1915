ec9204a5360ff7be89234be4684d4ee3c82ffe12
alson-ang <alson.ang@computhink.com.sg>
2026-09-10 12:39:42 +0000

    Sep 10, 2026, 12:39 PM

--- 03_handpose/handpose_game.js
+++ 03_handpose/handpose_game.js
@@ -107,6 +107,7 @@ function draw() {
     textAlign(CENTER, CENTER); // horizontal, vertical alignment
     fill("rgb(0, 255, 20)");
     text("Bounce the Ball", width / 2, height * 0.1); // (text, x, y)
+    fill("rgb(0, 255, 50)");
     textSize(32);
     text("Use Index Finger to bounce the Ball!", width / 2, height * 0.2);
   }
