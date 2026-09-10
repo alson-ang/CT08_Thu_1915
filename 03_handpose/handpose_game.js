20f927af60e2cb2b69b08d156c703780088143b4
alson-ang <alson.ang@computhink.com.sg>
2026-09-10 12:07:38 +0000

    Sep 10, 2026, 12:07 PM

--- 03_handpose/handpose_game.js
+++ 03_handpose/handpose_game.js
@@ -7,7 +7,7 @@ let hands = []; // global variable to store hands
 // Game sprites
 let fingerTip;
 let balloon;
-let leftWall, rightWall, topWAll,
+let leftWall, rightWall, topWall, botWall;
 
 function preload() {
   // Create options for model settings
