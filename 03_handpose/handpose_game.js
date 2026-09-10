0dcd6be4587a8e9f23e6a3bf79481a33576839f3
alson-ang <alson.ang@computhink.com.sg>
2026-09-10 12:31:08 +0000

    Sep 10, 2026, 12:31 PM

--- 03_handpose/handpose_game.js
+++ 03_handpose/handpose_game.js
@@ -105,7 +105,7 @@ function draw() {
   if (gameStarted === false) {
     textSize(28);
     textAlign(CENTER, CENTER); // horizontal, vertical alignment
-    
+    fill()
   }
 
   // Draw all the tracked hand points
