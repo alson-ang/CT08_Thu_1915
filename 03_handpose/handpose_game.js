f27d6da6fd4d9a261f71b46f17c4711bd053894e
alson-ang <alson.ang@computhink.com.sg>
2026-09-10 12:26:59 +0000

    Sep 10, 2026, 12:26 PM

--- 03_handpose/handpose_game.js
+++ 03_handpose/handpose_game.js
@@ -101,6 +101,10 @@ function draw() {
   // Draw the webcam video
   image(video, 0, 0, videoW, videoH);
 
+  if (gameStarted === false) {
+    
+  }
+
   // Draw all the tracked hand points
   // Loop through all the hands detected (can detect left or right)
   for (let i = 0; i < hands.length; i++) {
@@ -112,10 +116,6 @@ function draw() {
     // make sprite follow finger tip position
     fingerTip.x = keypoint.x;
     fingerTip.y = keypoint.y;
-  }
-
-  if (gameStarted === false) {
-    
   }
 }
 
