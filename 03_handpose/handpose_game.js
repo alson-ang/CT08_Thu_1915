7e83e1bc39940656eea1073b3555d84aacbf812c
alson-ang <alson.ang@computhink.com.sg>
2026-09-10 12:04:40 +0000

    Sep 10, 2026, 12:04 PM

--- 03_handpose/handpose_game.js
+++ 03_handpose/handpose_game.js
@@ -74,7 +74,7 @@ function draw() {
     // Keypoint 8 = INDEX_FINGER_TIP
     let keypoint = hand.keypoints[8];
 
-    // for every keypoint, draw a circle.
+    // draw sprite at finger tip
     fingerTip.x = keypoint.x;
     fingerTip.y = keypoint.y;
   }
