7035893438e6091b5602619fa88b92336d0df8a4
alson-ang <alson.ang@computhink.com.sg>
2026-09-10 12:38:47 +0000

    Sep 10, 2026, 12:38 PM

--- 03_handpose/handpose_game.js
+++ 03_handpose/handpose_game.js
@@ -108,7 +108,7 @@ function draw() {
     fill("rgb(0, 255, 20)");
     text("Bounce the Ball", width / 2, height * 0.1); // (text, x, y)
     textSize(32);
-    
+    text("Use")
   }
 
   // Draw all the tracked hand points
